1e5dc3651baf12f5f459db818a9ae6756d08fff2
Jack <jack.cohen1997@gmail.com>
2018-11-15 16:52:37 +0200

    completed cocktail logic

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -10,11 +10,29 @@ class DosesController < ApplicationController
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
-    names = params[:dose][:ingredient][1..-1]
-    names.each do |name|
-      ingredient = Ingredient.find_or_create_by(name: name)
-      Dose.create(cocktail: @cocktail, ingredient: ingredient)
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
     end
-    redirect_to @cocktail
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
+
+    # @dose = Dose.new(dose_params)
+    # @cocktail = Cocktail.find(params[:cocktail_id])
+    # names = params[:dose][:ingredient][1..-1]
+    # names.each do |name|
+    #   ingredient = Ingredient.find_or_create_by(name: name)
+    #   Dose.create(cocktail: @cocktail, ingredient: ingredient)
